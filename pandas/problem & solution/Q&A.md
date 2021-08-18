ea6375c5b65e3c1b1ac82380bf399ba98974340f
mraihan19 <rbfahim.iub13@outlook.com>
2021-08-18 10:34:49 +0600

    ✨ Question & Answer(Q1)

---
+++ pandas/problem & solution/Q&A.md
@@ -0,0 +1,5 @@
+| Question | Answer |   |   |   |
+|----|--------|---|---|---|
+|  1. What is the difference in final grade in Math for male vs female students?  |  Link      |   |   |   |
+|    |        |   |   |   |
+|    |        |   |   |   |
---
+++ pandas/problem & solution/Q1.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from pandas import read_excel
+import numpy as np
+
+df = pd.read_excel('mahim.xlsx')
+
+df_gender = pd.read_excel('mahim.xlsx', 'Dataset-student')
+df_math = pd.read_excel('mahim.xlsx', 'Dataset-student-math')
+
+
+
+female_idx = (df.index[df_gender['sex'] == 'F'].tolist())
+male_idx = (df.index[df_gender['sex'] == 'M'].tolist())
+print(female_idx)
+print(male_idx)
+
+m = 0
+for l in male_idx:
+    a = df_math.loc[l, 'G3']
+    m = m + a
+print(m)
+
+f = 0
+for l in female_idx:
+    a = df_math.loc[l, 'G3']
+    f = f + a
+print(f)
+
+diff = m-f
+print(diff)
